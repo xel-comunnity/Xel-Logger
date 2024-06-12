399807e48a726bf21ddb6e72b402f5980df82a92
bxel07 <yogipratama.ut@gmail.com>
2024-06-12 18:26:08 +0700

    bump: auto flush

--- src/ApplicationLogger.php
+++ src/ApplicationLogger.php
@@ -6,53 +6,90 @@ use Monolog\Handler\FirePHPHandler;
 
 class ApplicationLogger
 {
+    private static array $loggers = [];
 
-    public Logger $logger;
-
-    public function __construct(protected array $setup, protected FirePHPHandler $firePHPHandler)
-    {}
-
-
-    public function launch(): void
+    public static function launch(array $setup, FirePHPHandler $firePHPHandler): Logger
     {
-        if ($this->setup["mode"] !== "single") {
-            foreach ($this->setup["collections"] as $setup){
-                // ? logger instance
-                $this->logger = $setup['channel'];
-                // ? Rotating Handler
-                $loggingGeneration = new RotatingFileHandler
-                (
-                    $setup["path"],
-                    $setup["logging_generation"]["max"],
-                    $setup["level"],
-                    true,
-                    0644,
-                    false,
-                    $setup["logging_generation"]["format"],
-                );
+        $key = serialize($setup);
 
-                $this->logger->pushHandler($loggingGeneration);
-                $this->logger->pushHandler($this->firePHPHandler);
-            }
-        } else {
-            foreach ($this->setup["collections"] as $setup){
-                // ? logger instance
-                $this->logger = $setup['channel'];
-                // ? Rotating Handler
-                $loggingGeneration = new RotatingFileHandler
-                (
-                    $this->setup['path'],
-                    $this->setup['single_logging_generation']['max'],
-                    $setup["level"],
-                    true,
-                    0644,
-                    false,
-                    $this->setup['single_logging_generation']['format'],
-                );
+        if (!isset(self::$loggers[$key])) {
+            $logger = new Logger($setup['name']);
+            $logger->pushHandler($firePHPHandler);
+            $logger->pushHandler(self::createRotatingFileHandler($setup));
 
-                $this->logger->pushHandler($loggingGeneration);
-                $this->logger->pushHandler($this->firePHPHandler);
-            }
+            self::$loggers[$key] = $logger;
         }
+
+        return self::$loggers[$key];
     }
+
+    private static function createRotatingFileHandler(array $setup): RotatingFileHandler
+    {
+        $loggingGeneration = $setup['mode'] === 'single'
+            ? $setup['single_logging_generation']
+            : $setup['logging_generation'];
+
+        return new RotatingFileHandler(
+            $setup['path'],
+            $loggingGeneration['max'],
+            $setup['level'],
+            true,
+            0644,
+            false,
+            $loggingGeneration['format']
+        );
+    }
+
+    public static function flush(): void
+    {
+        self::$loggers = [];
+    }
+//    public Logger $logger;
+//
+//    public function __construct(protected array $setup, protected FirePHPHandler $firePHPHandler)
+//    {}
+//
+//
+//    public function launch(): void
+//    {
+//        if ($this->setup["mode"] !== "single") {
+//            foreach ($this->setup["collections"] as $setup){
+//                // ? logger instance
+//                $this->logger = $setup['channel'];
+//                // ? Rotating Handler
+//                $loggingGeneration = new RotatingFileHandler
+//                (
+//                    $setup["path"],
+//                    $setup["logging_generation"]["max"],
+//                    $setup["level"],
+//                    true,
+//                    0644,
+//                    false,
+//                    $setup["logging_generation"]["format"],
+//                );
+//
+//                $this->logger->pushHandler($loggingGeneration);
+//                $this->logger->pushHandler($this->firePHPHandler);
+//            }
+//        } else {
+//            foreach ($this->setup["collections"] as $setup){
+//                // ? logger instance
+//                $this->logger = $setup['channel'];
+//                // ? Rotating Handler
+//                $loggingGeneration = new RotatingFileHandler
+//                (
+//                    $this->setup['path'],
+//                    $this->setup['single_logging_generation']['max'],
+//                    $setup["level"],
+//                    true,
+//                    0644,
+//                    false,
+//                    $this->setup['single_logging_generation']['format'],
+//                );
+//
+//                $this->logger->pushHandler($loggingGeneration);
+//                $this->logger->pushHandler($this->firePHPHandler);
+//            }
+//        }
+//    }
 }
--- src/Loggers.php
+++ src/Loggers.php
@@ -6,75 +6,158 @@ use Monolog\Logger;
 
 class Loggers extends ApplicationLogger implements LoggerApp
 {
-    private Logger $channel;
-
+    private Logger $logger;
     public function __construct(array $setup, FirePHPHandler $firePHPHandler)
     {
-        parent::__construct($setup, $firePHPHandler);
-        $this->channel = $this->setup["collections"]['my_logger']['channel'];
+        $this->logger = ApplicationLogger::launch($setup, $firePHPHandler);
+    }
 
+
+    /**
+     * @inheritDoc
+     */
+    public function debug(array|int|string $debug, array $context = []): void
+    {
+        $this->logger->debug($debug, $context);
     }
 
     /**
-     * @param string $channel
-     * @return Loggers
+     * @inheritDoc
      */
-
-    public function channel(string $channel): Loggers
-    {
-        $this->channel = $this->setup["collections"][$channel]['channel'];
-        return $this;
-    }
-
-    public function debug(array|int|string $debug, array $context = []): void
-    {
-        $this->channel->debug($debug, $context);
-    }
     public function info(array|int|string $debug, array $context = []): void
     {
-        $this->channel->info($debug, $context);
+        $this->logger->info($debug, $context);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function notice(array|int|string $debug, array $context = []): void
     {
-
-        $this->channel->notice($debug, $context);
+        $this->logger->notice($debug, $context);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function warning(array|int|string $debug, array $context = []): void
     {
-        $this->channel->notice($debug, $context);
+        $this->logger->warning($debug, $context);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function error(array|int|string $debug, array $context = []): void
     {
-        $this->channel->error($debug, $context);
+        $this->logger->error($debug, $context);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function critical(array|int|string $debug, array $context = []): void
     {
-        $this->channel->critical($debug, $context);
+        $this->logger->critical($debug, $context);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function alert(array|int|string $debug, array $context = []): void
     {
-        $this->channel->alert($debug, $context);
+        $this->logger->alert($debug, $context);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function emergency(array|int|string $debug, array $context = []): void
     {
-        $this->channel->emergency($debug, $context);
+        $this->logger->emergency($debug, $context);
     }
 
     public function __destruct()
     {
-        // Close any resources if necessary, such as file handlers
-        foreach ($this->channel->getHandlers() as $handler) {
+        foreach ($this->logger->getHandlers() as $handler) {
             if (method_exists($handler, 'close')) {
                 $handler->close();
             }
         }
     }
+
+
+
+    //    private Logger $channel;
+//
+//    public function __construct(array $setup, FirePHPHandler $firePHPHandler)
+//    {
+//        parent::__construct($setup, $firePHPHandler);
+//        $this->channel = $this->setup["collections"]['my_logger']['channel'];
+//
+//    }
+//
+//    /**
+//     * @param string $channel
+//     * @return Loggers
+//     */
+//
+//    public function channel(string $channel): Loggers
+//    {
+//        $this->channel = $this->setup["collections"][$channel]['channel'];
+//        return $this;
+//    }
+//
+//    public function debug(array|int|string $debug, array $context = []): void
+//    {
+//        $this->channel->debug($debug, $context);
+//    }
+//    public function info(array|int|string $debug, array $context = []): void
+//    {
+//        $this->channel->info($debug, $context);
+//    }
+//
+//    public function notice(array|int|string $debug, array $context = []): void
+//    {
+//
+//        $this->channel->notice($debug, $context);
+//    }
+//
+//    public function warning(array|int|string $debug, array $context = []): void
+//    {
+//        $this->channel->notice($debug, $context);
+//    }
+//
+//    public function error(array|int|string $debug, array $context = []): void
+//    {
+//        $this->channel->error($debug, $context);
+//    }
+//
+//    public function critical(array|int|string $debug, array $context = []): void
+//    {
+//        $this->channel->critical($debug, $context);
+//    }
+//
+//    public function alert(array|int|string $debug, array $context = []): void
+//    {
+//        $this->channel->alert($debug, $context);
+//    }
+//
+//    public function emergency(array|int|string $debug, array $context = []): void
+//    {
+//        $this->channel->emergency($debug, $context);
+//    }
+//
+//    public function __destruct()
+//    {
+//        // Close any resources if necessary, such as file handlers
+//        foreach ($this->channel->getHandlers() as $handler) {
+//            if (method_exists($handler, 'close')) {
+//                $handler->close();
+//            }
+//        }
+//    }
+
 }
 
 
